8bdd773039e805184c979d65307dba2543f0e6e3
JeffCord <jeffcord97@gmail.com>
2020-02-27 21:14:32 -0600

    url parsing and urllib added to reqs.txt

--- pools.py
+++ pools.py
@@ -2,6 +2,7 @@ from flask import Flask, request as flask_request
 import requests
 import xml.etree.ElementTree as ET
 import json
+import urllib.parse
 
 app = Flask(__name__)
 etags = []
@@ -32,8 +33,8 @@ def find_all_pools():
     # with open ('all_pools.json', 'w') as f:
     #     new_json = json.dump(pools_dict, f, indent=2)
 
-    # return json.dumps(pools_dict, indent=2)
-    return pools_dict
+    return json.dumps(pools_dict)
+    # return pools_dict
 
 
 # returns json data for a requested pool
@@ -48,8 +49,11 @@ def find_pool(pool_name):
     # check for ETAG
     etag_header = flask_request.headers.get('If-None-Match')
 
-    if etag_header is not None and etag_header in etags:
-        return pool_name, 304
+    if etag_header is not None:
+        etag_header = urllib.parse.unquote(etag_header)
+        print(etag_header)
+        if etag_header in etags:
+            return '', 304
 
     url_xml_data = 'https://raw.githubusercontent.com/devdattakulkarni/elements-of-web-programming' \
                    '/master/data/austin-pool-timings.xml'
@@ -70,7 +74,7 @@ def find_pool(pool_name):
             # if pool is found, create a dictionary of that pool's data,
             # and break out of the loop
             if j.tag == 'pool_name' and j.text == pool_name:
-                pool_dict = create_dict(pool_name, root[i])
+                pool_dict = create_dict(root[i])
                 pool_found = True
                 break
 
@@ -90,7 +94,7 @@ def find_pool(pool_name):
     return pool_dict, 200, {'ETag': pool_name}
 
 
-def create_dict(pool_name, xml_element):
+def create_dict(xml_element):
     result_dict = {}
     wanted_tags = ['pool_name', 'status', 'phone', 'pool_type']
 
